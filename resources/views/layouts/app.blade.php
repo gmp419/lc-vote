d059d0268de2fd0417dea184cfa89105ff149c88
gaurang <patel7483g@gmail.com>
2022-04-28 17:34:34 -0300

    Finish add idea card design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,8 +50,47 @@
     </header>
 
     <main class="container mx-auto max-w-custom flex">
-        <div class="w-70 mr-5">
-            An idea for form goes here
+        <div class="w-72 mr-5 cursor-pointer">
+            <div class="border bg-white border-blue rounded-xl mt-16" style="
+                           border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+                             border-image-slice: 1;
+                             background-image: linear-gradient(to bottom, #ffffff, #ffffff), linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+                             background-origin: border-box;
+                             background-clip: content-box, border-box;
+                     ">
+                <div class="text-center px-6 py-2 pt-6">
+                    <h3 class="font-semibold text-base">Add an idea</h3>
+                    <p class="mt-4 text-xs">Let us know what would you like and we'll take a look over!</p>
+
+                </div>
+                <form action="#" method="POST" class="space-y-4  px-4 py-6">
+                    <div>
+                        <input type="text" class="bg-gray-100 text-sm border-none w-full rounded-xl placeholder-gray-900 px-4 py-2 " placeholder="Your idea">
+                    </div>
+                    <div>
+                        <select name="cateogory_add" id="category_add" class="w-full bg-gray-100 text-sm rounded-xl border-none px-4 py-2">
+                            <option value="category one">Category One</option>
+                            <option value="category two">Category Two</option>
+                            <option value="category three">Category Three</option>
+                            <option value="category four">Category Four</option>
+                        </select>
+                    </div>
+                    <div>
+                        <textarea name="idea" id="idea" cols="30" rows="4" class="text-sm bg-gray-100 w-full rounded-xl placeholder-gray-900 px-4 py-2 border-none ">Describe your idea</textarea>
+                    </div>
+                    <div class="flex items-center justify-between space-x-3 overflow-hidden">
+                        <button type="button" class="flex items-center justify-center h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in py-3 px-4">
+                            <svg class="w-4 transform -rotate-45 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                            </svg>
+                            <span class="">Attach</span>
+                        </button>
+                        <button type="submit" class="flex items-center justify-center w-full h-11 text-xs bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in py-3 px-4">
+                            <span class="text-white">Submit</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
         <div class="w-175">
             <nav class="flex items-center justify-between text-xs">
